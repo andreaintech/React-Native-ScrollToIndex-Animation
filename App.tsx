037a81c25d9e13c1f84c0b24505f09d890991c29
Andrea Da Silva <adasilvapdev@gmail.com>
2022-01-04 19:30:11 -0400

    initial commit :)

---
+++ App.tsx
@@ -0,0 +1,195 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, StyleSheet, Dimensions, FlatList, TouchableOpacity } from 'react-native';
+// import { MotiView } from '@motify/components';
+import faker from 'faker';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Feather from 'react-native-vector-icons/Feather';
+
+const { width, height } = Dimensions.get('screen');
+
+faker.seed(10);
+
+const data = [...Array(20).keys()].map(() => ({
+  key: faker.datatype.uuid(),
+  job: faker.animal.crocodilia(),
+}));
+
+const _colors = {
+  active: `#FCD259ff`,
+  inactive: `#FCD25900`,
+};
+const _noSpacing = 0;
+const _spacing = 10;
+
+const App = () => {
+  const ref = useRef<FlatList>(null);
+  const [index, setIndex] = useState(0);
+  const [viewPosition, setViewPosition] = useState(0);
+
+  useEffect(() => {
+    ref.current?.scrollToIndex({
+      animated: true,
+      index,
+      viewOffset: viewPosition === 0.5 || viewPosition === 1 ? _noSpacing : _spacing,
+      viewPosition,  // percentage from the viewPort starting from left hanside
+    });
+  }, [index, viewPosition]);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <FlatList
+        ref={ref}
+        initialScrollIndex={index}
+        style={{ flexGrow: 0 }}
+        data={data}
+        keyExtractor={(item) => item.key}
+        contentContainerStyle={{ paddingLeft: _spacing }}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        renderItem={({ item, index: fIndex }) => {
+          return (
+            <TouchableOpacity onPress={() => {
+              setIndex(fIndex);
+            }}>
+              <View
+                style={{
+                  marginRight: _spacing,
+                  padding: _spacing,
+                  borderWidth: 2,
+                  borderColor: _colors.active,
+                  borderRadius: 12,
+                  backgroundColor: fIndex === index ? _colors.active : _colors.inactive,
+                }}>
+                <Text style={{ color: '#36303F', fontWeight: '700' }}>
+                  {item.job}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      />
+      <View
+        style={{
+          alignItems: 'center',
+          flexDirection: 'row',
+          marginTop: _spacing * 10,
+        }}>
+        <View style={{ alignItems: 'center' }}>
+          <Text
+            style={{
+              color: '#36303F',
+              fontWeight: '700',
+              marginBottom: _spacing,
+            }}>
+            Scroll position
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              width: width / 2,
+              justifyContent: 'center',
+            }}>
+            <TouchableOpacity onPress={() => {
+              setViewPosition(0);
+            }}>
+              <View
+                style={{
+                  padding: _spacing,
+                  backgroundColor: '#FCD259',
+                  borderRadius: _spacing,
+                  marginRight: _spacing,
+                }}>
+
+                <Entypo name='align-left' size={24} color='#36303F' />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+              setViewPosition(0.5);
+            }}>
+              <View
+                style={{
+                  padding: _spacing,
+                  backgroundColor: '#FCD259',
+                  borderRadius: _spacing,
+                  marginRight: _spacing,
+                }}>
+                <Entypo
+                  name='align-horizontal-middle'
+                  size={24}
+                  color='#36303F'
+                />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+              setViewPosition(1);
+            }}>
+              <View
+                style={{
+                  padding: _spacing,
+                  backgroundColor: '#FCD259',
+                  borderRadius: _spacing,
+                }}>
+                <Entypo name='align-right' size={24} color='#36303F' />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <Text
+            style={{ color: '#36303F', fontWeight: '700', marginBottom: 10 }}>
+            Navigation
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              width: width / 2,
+              justifyContent: 'center',
+            }}>
+            <TouchableOpacity onPress={() => {
+              if (index === 0)
+                return;
+
+              setIndex(index - 1);
+            }}>
+              <View
+                style={{
+                  padding: _spacing,
+                  backgroundColor: '#FCD259',
+                  borderRadius: _spacing,
+                  marginRight: _spacing,
+                }}>
+                <Feather name='arrow-left' size={24} color='#36303F' />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+              if (index === data.length - 1)
+                return;
+
+              setIndex(index + 1);
+            }}>
+              <View
+                style={{
+                  padding: _spacing,
+                  backgroundColor: '#FCD259',
+                  borderRadius: _spacing,
+                }}>
+                <Feather name='arrow-right' size={24} color='#36303F' />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+  },
+});
+
+export default App;
